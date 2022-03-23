76774234bce8e78801355aedf8c811a563d65178
Matheus Amorim <matheus.amorim02@gmail.com>
2022-03-23 13:47:03 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha =  "senhaSuperSegura456!";
+const StringDeNumeros = "34561656";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo";
+
+console.log(citacao)
+
+// concatenação
+
+console.log(citacao + meuNome)
+
+//template String ou Template Literal
