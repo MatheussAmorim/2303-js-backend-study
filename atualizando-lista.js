4c0cf82da323734ce223fbb851075035a722380f
Matheus Amorim <matheus.amorim02@gmail.com>
2022-03-24 16:01:49 -0300

    callbacks

--- atualizando-lista.js
+++ atualizando-lista.js
@@ -1,5 +1,5 @@
 const listaDeChamada = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie']
 
-listaDeChamada.splice(1, 2, 'Rodrigo')
+listaDeChamada.splice(1, 2, 'Rodrigo') // remove pelos dois primeiros parametros e insere no último 
 
 console.log(listaDeChamada)
--- localizando-aluno.js
+++ localizando-aluno.js
@@ -4,8 +4,8 @@ const medias = [10, 7, 9, 6]
 let listaDeNotasEAlunos = [alunos, medias]
 
 const exibeNomeNota = (nomeDoAluno) => {
-    if(listaDeNotasEAlunos[0].includes(nomeDoAluno)){
-        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno)
+    if(listaDeNotasEAlunos[0].includes(nomeDoAluno)){ // verifica se o nome está na lista
+        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno) // pega o indice do array
         return listaDeNotasEAlunos[0][indice] + ', sua média é ' + listaDeNotasEAlunos[1][indice]
     } else {
         return "Aluno não está cadastrado"
