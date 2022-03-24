f7fe1338a0d4a837e229c94335a20f7e302a2e06
Matheus Amorim <matheus.amorim02@gmail.com>
2022-03-24 15:05:19 -0300

    atualizando listas

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const sala1 = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie']
+const sala2 = ['Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo']
+
+const salaUnificada = sala1.concat(sala2)
+
+console.log(salaUnificada)
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio']
+const medias = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, medias]
+
+console.log(listaDeNotasEAlunos)
